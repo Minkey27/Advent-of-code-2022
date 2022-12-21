ef2793a93a75a762ef3354e545faa843d0e4c550
Yi Ming Yung <yiming.yung@outlook.com>
2022-12-21 16:30:12 +0100

    Day 9

--- Day 9/Part_1_solution.py
+++ Day 9/Part_1_solution.py
@@ -1,64 +1,41 @@
 import os
-import re
+from collections import namedtuple
 
 
-def print_playing_field(playing_field):
-    """
-    Util function to prettily print the playing field for ease of debugging.
-    """
-    for x in playing_field:
-        print(x)
+Point = namedtuple("Point", "x y")
+
+UP = Point(1, 0)
+RIGHT = Point(0, 1)
+DOWN = Point(-1, 0)
+LEFT = Point(0, -1)
+
+DIRECTIONS = {"U": UP, "R": RIGHT, "D": DOWN, "L": LEFT}
 
 
-def calculate_and_move_head_position(playing_field, direction, steps, current_head_x, current_head_y):
-    end_x = current_head_x
-    end_y = current_head_y
+def move_point(point_a, point_b):
+    return Point(point_a.x + point_b.x, point_a.y + point_b.y)
 
-    for step in range(steps):
-        if direction == "U":
-            end_x -= 1
-        elif direction == "R":
-            end_y += 1
-        elif direction == "D":
-            end_x += 1
-        elif direction == "L":
-            end_y -= 1
 
-        playing_field[end_x][end_y] = "H"
-
-    print_playing_field(playing_field)
-    return end_x, end_y
+def sign(x):
+    return -1 if x < 0 else 1 if x > 0 else 0
 
 
 if __name__ == "__main__":
     f = open(os.path.join("Day 9", "puzzle_input.txt"), "r")
     puzzle_input = f.read().splitlines()
 
-    # Generate playing field to visualize steps and ease of debugging.
-    playing_field = []
-    for x in range(0, 25):
-        playing_field.append(["." for x in range(0, 25)])
+    rope = [Point(0, 0)] * 2
+    visited = set(rope)
+    for command in puzzle_input:
+        direction, steps = command.split(" ")
 
-    # Set starting point.
-    current_head_x = 20
-    current_head_y = 15
-    playing_field[current_head_x][current_head_y] = "H"
+        for step in range(int(steps)):
+            rope[1] = move_point(rope[1], DIRECTIONS[direction])
 
-    print_playing_field(playing_field)
+            delta = Point(rope[1].x - rope[0].x, rope[1].y - rope[0].y)
+            if abs(delta.x) > 1 or abs(delta.y) > 1:
+                rope[0] = Point(rope[0].x + sign(delta.x), rope[0].y + sign(delta.y))
 
-    for move_command in puzzle_input[:50]:
-        move_command = re.search(r"(.) (\d*)", move_command)
+            visited.add(rope[0])
 
-        direction = move_command[1]
-        steps = int(move_command[2])
-
-        import pdb; pdb.set_trace()
-        print(direction, steps)
-        print(current_head_x, current_head_y)
-        current_head_x, current_head_y = calculate_and_move_head_position(
-            playing_field,
-            direction,
-            steps,
-            current_head_x,
-            current_head_y,
-        )
+    print(len(visited))
--- Day 9/Part_2_solution.py
+++ Day 9/Part_2_solution.py
@@ -1,6 +1,42 @@
 import os
+from collections import namedtuple
+
+
+Point = namedtuple("Point", "x y")
+
+UP = Point(1, 0)
+RIGHT = Point(0, 1)
+DOWN = Point(-1, 0)
+LEFT = Point(0, -1)
+
+DIRECTIONS = {"U": UP, "R": RIGHT, "D": DOWN, "L": LEFT}
+
+
+def move_point(point_a, point_b):
+    return Point(point_a.x + point_b.x, point_a.y + point_b.y)
+
+
+def sign(x):
+    return -1 if x < 0 else 1 if x > 0 else 0
 
 
 if __name__ == "__main__":
     f = open(os.path.join("Day 9", "puzzle_input.txt"), "r")
     puzzle_input = f.read().splitlines()
+
+    rope = [Point(0, 0)] * 10
+    visited = set(rope)
+    for command in puzzle_input:
+        direction, steps = command.split(" ")
+
+        for step in range(int(steps)):
+            rope[0] = move_point(rope[0], DIRECTIONS[direction])
+
+            for i in range(1, 10):
+                delta = Point(rope[i - 1].x - rope[i].x, rope[i - 1].y - rope[i].y)
+                if abs(delta.x) > 1 or abs(delta.y) > 1:
+                    rope[i] = Point(rope[i].x + sign(delta.x), rope[i].y + sign(delta.y))
+
+                visited.add(rope[-1])
+
+    print(len(visited))
